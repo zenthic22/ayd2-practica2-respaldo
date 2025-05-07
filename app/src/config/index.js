45e19f1cd24530b04348ff22287876c1e38bf619
Marco Mazariegos <marcomazariegos18@gmail.com>
2025-05-07 16:26:04 -0600

    fixed url

--- app/src/config/index.js
+++ app/src/config/index.js
@@ -1,4 +1,4 @@
 const { protocol, hostname } = window.location;
 export const WEATHER_SERVICE_URL = `http://44.208.93.157:3001`;
 export const TEMP_SERVICE_URL = `http://3.214.77.39:3000`;
-export const AIR_SERVICE_URL = `http//52.3.190.223:3002`;
+export const AIR_SERVICE_URL = `http://52.3.190.223:3002`;
